94080fd584884214165a7685728e0677a4af5c6e
Bruce Shen <98313918+ChaicBlack@users.noreply.github.com>
2024-04-27 22:37:29 +0100

    changed

--- d-way_heap/d-way_heap.cpp
+++ d-way_heap/d-way_heap.cpp
@@ -11,17 +11,31 @@ public:
   DHeap() = default;
 
   // index通常是假定比parent优先级大的children
-  void bubbleup(std::vector<std::pair<std::string, int>> pairs, int index) {
+  void bubbleUp(std::vector<std::pair<std::string, int>> pairs, int index) {
     auto current = pairs[index];
     while (index > 0) {
-      int parentIndex =
-          (parentIndex - 1) / D; // 或者是任何获得其parent的实现方式
-      if (pairs[parentIndex].second < current.second) {
-        pairs[index] = pairs[parentIndex];
-        index = parentIndex;
+      int parentIdx =
+          (parentIdx - 1) / D; // 或者是任何获得其parent的实现方式
+      if (pairs[parentIdx].second < current.second) {
+        pairs[index] = pairs[parentIdx];
+        index = parentIdx;
       } else
         break;
     }
     pairs[index] = current;
+  }
+
+  void pushDown(std::vector<std::pair<std::string, int>> pairs, int index = 0){
+    auto current = pairs[index];
+    auto currentIdx = index;
+    while(currentIdx < (pairs.size() - 2) / D + 1){ // (|pairs|-2)/D+1是第一个叶子的序号
+      auto [child, childIdx] = highestPriorityChild(currentIdx);
+      if(child.second > current.second){
+        pairs[currentIdx] = child;
+        currentIdx = childIdx;
+      } else
+        break;
+    }
+    pairs[currentIdx] = current;
   }
 };
